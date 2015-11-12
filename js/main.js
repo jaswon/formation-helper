30eb63fc99e15040183d56fcca385635830f1a83
Jason W <jasonwon628@gmail.com>
2015-11-12 03:13:53 -0500

    rename vars

--- js/main.js
+++ js/main.js
@@ -1,29 +1,42 @@
-var formations;
+var projects;
 
 $(function() {
     // simpleStorage.set('list',JSON.stringify([1,2,3,4,5,6,7,8,9,10]))
     var getlist = simpleStorage.get('list')
     if (getlist === undefined) {
         simpleStorage.set('list',"[]")
-        formations = []
+        projects = []
     } else {
-        formations = JSON.parse(getlist)
+        projects = JSON.parse(getlist)
     }
     startMenu()
 })
 
 function startMenu () {
-    formations.forEach(function(i) {
-        $('#formations').append($( "<a/>", {
+    projects.forEach(function(i) {
+        $('#project-list').append($( "<a/>", {
             class: "list-group-item",
             href: "#",
+            click: function() {
+                openProject(i)
+            },
             text: i
         }))
     })
-    $('#formations').append($( "<a/>", {
+    $('#project-list').append($( "<a/>", {
         class: "list-group-item",
         href: "#",
+        click: function() {
+            openProject("new")
+        },
         text: "+ Create new project"
     }))
-    $("#myModal").modal('show');
+    $("#project-modal").modal({
+        backdrop: 'static'
+    });
+}
+
+function openProject (project) {
+    $("#project-modal").modal('hide')
+    $('.workspace').text(project)
 }
